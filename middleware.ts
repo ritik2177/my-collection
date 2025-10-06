fcb1dae9dd84463be4065a29796a36fafc09658f
ritik2177 <ritikkatsa2005@gmail.com>
2025-10-06 22:43:24 +0530

    update

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,20 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   const isLoggedIn =
-//     request.cookies.get("next-auth.session-token") ||
-//     request.cookies.get("__Secure-next-auth.session-token");
+export function middleware(request: NextRequest) {
+  const isLoggedIn =
+    request.cookies.get("next-auth.session-token") ||
+    request.cookies.get("__Secure-next-auth.session-token");
 
-//   if (!isLoggedIn) {
-//     return NextResponse.redirect(new URL("/", request.url));
-//   }
+  if (!isLoggedIn) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ["/dashboard", "/dashboard/:path*", "/owner/:path*"],
-// };
+export const config = {
+  matcher: [ "/owner/:path*"],
+};
+
+// "/dashboard", "/dashboard/:path*",
